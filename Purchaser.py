19152154330bfab7916ef8d67e19aebc359039c6
Harkiran Grewal <harkiran988@gmail.com>
2022-03-19 20:54:44 -0400

    Pub/priv key initialization and write to file
    Initialized the public and private keys of Purchaser.py and wrote the public key to a file.

--- Purchaser.py
+++ Purchaser.py
@@ -7,6 +7,26 @@ Created on Sat Mar 19
 
 #List of imports needed for the code 
 import socket 
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+#Initailize the RSA key
+key = RSA.generate(2048)
+
+#Generate private key 
+private_key = key.export_key()
+priv_key_purch = RSA.importKey(private_key)
+priv_key_purch = PKCS1_OAEP.new(priv_key_purch)
+    
+#Generate public key 
+public_key = key.publickey().export_key()
+pub_key_purch = RSA.importKey(public_key)
+public_key_purch = PKCS1_OAEP.new(pub_key_purch)
+    
+#Write the public key to the .pem file, accessible by Supervisor and Purchaser
+file_out = open("purchaser_public_key.pem", "wb")
+file_out.write(public_key)
+file_out.close()
 
 #The purchaser acts as a client to the OrderDept socket, and as a server to the Supervisor
 #Setting up the server socket 
