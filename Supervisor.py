8ca4967ce0d7370a57950c78df66261728147eab
Harkiran Grewal <harkiran988@gmail.com>
2022-03-19 20:35:40 -0400

    Setting up connections in Supervisor
    Setting up connections for the supervisor to connect as a client to the orderdept and purchaser.

--- Supervisor.py
+++ Supervisor.py
@@ -7,3 +7,23 @@ Created on Sat Mar 19
 #List of imports needed for the code 
 import socket 
 
+#The supervisor is a client to the OrderDept socket and the Purchaser socket
+
+#Connecting to the OrderDept socket 
+s_order = socket.socket()             # Create a socket object
+port = 60000                    # Reserve a port for your service.
+
+s_order.connect(('127.0.0.1', port))
+
+#Connecting to the Purchaser socket 
+s_purch = socket.socket()             # Create a socket object
+port = 30000                    # Reserve a port for your service.
+
+s_purch.connect(('127.0.0.1', port))
+
+
+#Closing the s_order socket 
+#s_order.close()
+
+#Closing the s_purch socket
+#s_purch.close()
