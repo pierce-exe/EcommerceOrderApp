d61e60bccfda179eed38245f3e9b913b52a0e594
Harkiran Grewal <harkiran988@gmail.com>
2022-03-19 20:57:13 -0400

    Pub/priv key definitions and write to file
    Created definitions for the supervisor public and private key, and wrote the public key to a file.

--- Supervisor.py
+++ Supervisor.py
@@ -6,6 +6,26 @@ Created on Sat Mar 19
 """
 #List of imports needed for the code 
 import socket 
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+#Initailize the RSA key
+key = RSA.generate(2048)
+
+#Generate private key 
+private_key = key.export_key()
+priv_key_super = RSA.importKey(private_key)
+priv_key_super = PKCS1_OAEP.new(priv_key_super)
+    
+#Generate public key 
+public_key = key.publickey().export_key()
+pub_key_super = RSA.importKey(public_key)
+public_key_super = PKCS1_OAEP.new(pub_key_super)
+    
+#Write the public key to the .pem file, accessible by Supervisor and Purchaser
+file_out = open("supervisor_public_key.pem", "wb")
+file_out.write(public_key)
+file_out.close()
 
 #The supervisor is a client to the OrderDept socket and the Purchaser socket
 
